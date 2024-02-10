2161f8899de698b8088f9d54e426b1b287d376a5
nadimsvarun <nadimsvarun@gmail.com>
2024-02-10 19:25:19 +0530

    added testpage2

---
+++ src/main/java/com/qa/winnowpro/pages/TestPage2.java
@@ -0,0 +1,14 @@
+package com.qa.winnowpro.pages;
+
+import org.openqa.selenium.By;
+
+public class TestPage2 {
+	
+	By loc = By.id("order");
+	
+	public void getorde() {
+		
+		System.out.println("get order");
+	}
+
+}
