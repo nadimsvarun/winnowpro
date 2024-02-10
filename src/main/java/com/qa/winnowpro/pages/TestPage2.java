b8abf7f2cccc2517e74cf8fb0578aae0ad9303dc
nadimsvarun <nadimsvarun@gmail.com>
2024-02-10 19:54:37 +0530

    updated testpage2 as per comments from varun

--- src/main/java/com/qa/winnowpro/pages/TestPage2.java
+++ src/main/java/com/qa/winnowpro/pages/TestPage2.java
@@ -5,10 +5,16 @@ import org.openqa.selenium.By;
 public class TestPage2 {
 	
 	By loc = By.id("order");
+	By ploc = By.id("price");
 	
-	public void getorde() {
+	public void getorder() {
 		
 		System.out.println("get order");
+	}
+	
+	public void getprice() {
+		
+		System.out.println("get price");
 	}
 
 }
