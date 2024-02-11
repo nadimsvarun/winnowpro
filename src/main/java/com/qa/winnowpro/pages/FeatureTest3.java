b6f88fbeb3f303d8f394883377dfcf9d98fc8c1e
nadimsvarun <nadimsvarun@gmail.com>
2024-02-11 11:42:59 +0530

    featuretest3

---
+++ src/main/java/com/qa/winnowpro/pages/FeatureTest3.java
@@ -0,0 +1,11 @@
+package com.qa.winnowpro.pages;
+
+public class FeatureTest3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("f3 feature");
+	}
+
+}
